63fb952f8ed0528bf315b7fcd22f735142e07cdd
xiongxin <xiongxin@e-shion.net>
2016-12-13 17:00:13 +0800

    Initial commit

---
+++ src/main/java/com/daimajia/swipedemo/MyDemo.java
@@ -0,0 +1,17 @@
+package com.daimajia.swipedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by eshion on 16-11-2.
+ */
+public class MyDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo);
+
+    }
+}
